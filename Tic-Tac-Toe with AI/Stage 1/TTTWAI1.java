e5cd96c6df319f000beb366fd99f3b06d05e4644
GitHubRegisteredUser <131602241+GitHubRegisteredUser@users.noreply.github.com>
2023-06-12 21:58:01 +0300

    Stage 1

---
+++ Tic-Tac-Toe with AI/Stage 1/TTTWAI1.java
@@ -0,0 +1,129 @@
+package TicTacToeWithAI;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class TTTWAI1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        char[][] field = createField(input);
+        printField(field);
+        int xs = countX(input);
+        int os = countO(input);
+
+        while (true) {
+            try {
+                int m = scanner.nextInt();
+                int n = scanner.nextInt();
+                if (m > 3 || n > 3 || m < 1 || n < 1) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                } else {
+                    if (field[m - 1][n - 1] != '_') {
+                        System.out.println("This cell is occupied! Choose another one!");
+                    } else {
+                        if (xs == os) {
+                            field[m - 1][n - 1] = 'X';
+                            xs++;
+                            printField(field);
+                            if (winsX(field)) {
+                                System.out.println("X wins");
+                                break;
+                            }
+                        } else {
+                            field[m - 1][n - 1] = 'O';
+                            os++;
+                            printField(field);
+                            if (winsO(field)) {
+                                System.out.println("O wins");
+                                break;
+                            }
+                        }
+                        if (os + xs == 9) {
+                            System.out.println("Draw");
+                            break;
+                        } else {
+                            System.out.println("Game not finished");
+                        }
+                    }
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("You should enter numbers!");
+                scanner.next();
+            }
+        }
+    }
+
+    static char[][] createField(String s) {
+        char[][] array = new char[3][3];
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = s.charAt(counter++);
+            }
+        }
+        return array;
+    }
+
+    static void printField(char[][] array) {
+        System.out.println("---------");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+
+    static int countX(String s) {
+        int x = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'X') {
+                x++;
+            }
+        }
+        return x;
+    }
+
+    static int countO(String s) {
+        int o = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'O') {
+                o++;
+            }
+        }
+        return o;
+    }
+
+    static boolean winsX(char[][] array) {
+        if ((array[0][0] == 'X' && array[0][1] == 'X' && array[0][2] == 'X')
+                || (array[1][0] == 'X' && array[1][1] == 'X' && array[1][2] == 'X')
+                || (array[2][0] == 'X' && array[2][1] == 'X' && array[2][2] == 'X')
+                || (array[0][0] == 'X' && array[1][0] == 'X' && array[2][0] == 'X')
+                || (array[0][1] == 'X' && array[1][1] == 'X' && array[2][1] == 'X')
+                || (array[0][2] == 'X' && array[1][2] == 'X' && array[2][2] == 'X')
+                || (array[0][0] == 'X' && array[1][1] == 'X' && array[2][2] == 'X')
+                || (array[0][2] == 'X' && array[1][1] == 'X' && array[2][0] == 'X')) {
+            return true;
+        }
+        return false;
+    }
+
+    static boolean winsO(char[][] array) {
+        if ((array[0][0] == 'O' && array[0][1] == 'O' && array[0][2] == 'O')
+                || (array[1][0] == 'O' && array[1][1] == 'O' && array[1][2] == 'O')
+                || (array[2][0] == 'O' && array[2][1] == 'O' && array[2][2] == 'O')
+                || (array[0][0] == 'O' && array[1][0] == 'O' && array[2][0] == 'O')
+                || (array[0][1] == 'O' && array[1][1] == 'O' && array[2][1] == 'O')
+                || (array[0][2] == 'O' && array[1][2] == 'O' && array[2][2] == 'O')
+                || (array[0][0] == 'O' && array[1][1] == 'O' && array[2][2] == 'O')
+                || (array[0][2] == 'O' && array[1][1] == 'O' && array[2][0] == 'O')) {
+            return true;
+        }
+        return false;
+    }
+
+}
